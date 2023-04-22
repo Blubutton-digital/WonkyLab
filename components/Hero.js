6a1b58d2bce92a0d69d74b854656e310e98dec10
Joshua Gretton <jb.gretton@googlemail.com>
2023-04-22 18:00:38 +0100

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="w-screen h-screen bg-gray-700 relative">
+    <div className="w-screen h-screen bg-gray-700 relative duration-[999999ms]">
       <div className="container mx-auto h-full">
         <div className="max-w-xl flex items-center justify-center h-full px-4">
           <h2 className="font-semibold text-6xl font-playfair leading-normal text-white">
