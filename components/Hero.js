c156ff1c688ece148492a811197f7728fd729556
Joshua Gretton <jb.gretton@googlemail.com>
2023-05-08 09:44:58 +0100

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -1,9 +1,20 @@
 import React from "react";
+import Image from "next/image";
+import HeroBg from "../public/Images/Hero-bg.jpg";
 
 const Hero = () => {
   return (
-    <div className="w-screen h-screen bg-gray-700 relative">
-      <div className="container mx-auto h-full">
+    <div className="w-screen h-screen bg-black/50 relative">
+      <Image
+        src={HeroBg}
+        alt="Background image of the bar"
+        fill
+        className="object-cover -z-10"
+        quality={100}
+        priority
+      />
+
+      <div className="container mx-auto h-full z-50">
         <div className="max-w-xl flex items-center justify-center h-full px-4">
           <h2 className="font-semibold text-6xl font-playfair leading-normal text-white">
             Where Craft Meets Community
