ce7643755ac3de67d3846f447399cd5807f8a6e5
Joshua Gretton <jb.gretton@googlemail.com>
2023-06-27 20:42:17 +0100

    Update book.js

--- pages/book.js
+++ pages/book.js
@@ -145,7 +145,7 @@ const Book = () => {
               name="contact"
               method="POST"
               data-netlify="true"
-            // action="book?success=true"
+              action="/book"
             >
               {/* Name */}
               <div className="sm:col-span-3">
