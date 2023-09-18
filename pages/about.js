3254c8a3269eb6808a57a75d402897375dd9ccf3
Joshua Gretton <jb.gretton@googlemail.com>
2023-09-18 20:46:27 +0700

    Colour Update
    BG colour changed to match theme green. Text changed to white. Logo change

--- pages/about.js
+++ pages/about.js
@@ -12,87 +12,87 @@ import LogoList from "@/components/LogoList";
 import Banner from "@/components/Banner";
 
 const _about = () => {
-  return (
-    <div className="bg-[#EBEBEB]">
-      <header className="pb-20">
-        <NavBar />
-      </header>
-      <main className="lg:pt-40 sm:pt-12 pt-4">
-        <section className="container mx-auto flex flex-col lg:flex-row justify-between sm:mt-2 mt-10 px-4 md:pb-24 pb-14 gap-x-20">
-          <div className="lg:w-6/12 xl:w-4/12 w-full sm:h-[594px] h-[400px] relative mt-14 lg:mt-0 order-2 lg:order-1">
-            <Image
-              src={Macca}
-              alt="Photo of shop front"
-              className="h-full w-full object-cover lg:object-top rounded-md object-center"
-              fill
-              placeholder="blur"
-            />
-          </div>
-          <div className="lg:w-6/12 w-full h-full relative flex flex-col  my-auto order-1 lg:order-2">
-            <H3 black>Our Story</H3>
-            <p className="text-dark lg:text-xl text-lg mt-14 font-lato font-light tracking-wide lg:leading-relaxed leading-loose">
-              The new face of The Wonky Labrador is James “Mac” McGrath.
-              Village-boy from South Lincolnshire, Mac has fallen in love with
-              Sheffield & the Peak District. As a craft beer fanatic, and with
-              20 years experience in hospitality, opening a bar here was pretty
-              much inevitable.
-            </p>
-            <p className="text-dark lg:text-xl text-lg mt-14 font-lato font-light tracking-wide lg:leading-relaxed leading-loose">
-              We welcome boardgamers, beer aficionados, weekend
-              cocktail-drinkers, and pre-dinner wine-lovers, as well as hosting
-              private events and mental health safe-space evenings
-            </p>
-            <p className="text-dark lg:text-xl text-lg mt-14 font-lato font-light tracking-wide lg:leading-relaxed leading-loose">
-              To see our current offerings{" "}
-              <Link href={"/#offers"} className="text-wonky">
-                click here.
-              </Link>
-            </p>
-          </div>
-        </section>
+	return (
+		<div className="bg-[#EBEBEB]">
+			<header className="pb-20">
+				<NavBar />
+			</header>
+			<main className="lg:pt-40 sm:pt-12 pt-4">
+				<section className="container mx-auto flex flex-col lg:flex-row justify-between sm:mt-2 mt-10 px-4 md:pb-24 pb-14 gap-x-20">
+					<div className="lg:w-6/12 xl:w-4/12 w-full sm:h-[594px] h-[400px] relative mt-14 lg:mt-0 order-2 lg:order-1">
+						<Image
+							src={Macca}
+							alt="Photo of shop front"
+							className="h-full w-full object-cover lg:object-top rounded-md object-center"
+							fill
+							placeholder="blur"
+						/>
+					</div>
+					<div className="lg:w-6/12 w-full h-full relative flex flex-col  my-auto order-1 lg:order-2">
+						<H3 black>Our Story</H3>
+						<p className="text-dark lg:text-xl text-lg mt-14 font-lato font-light tracking-wide lg:leading-relaxed leading-loose">
+							The new face of The Wonky Labrador is James “Mac” McGrath.
+							Village-boy from South Lincolnshire, Mac has fallen in love with
+							Sheffield & the Peak District. As a craft beer fanatic, and with
+							20 years experience in hospitality, opening a bar here was pretty
+							much inevitable.
+						</p>
+						<p className="text-dark lg:text-xl text-lg mt-14 font-lato font-light tracking-wide lg:leading-relaxed leading-loose">
+							We welcome boardgamers, beer aficionados, weekend
+							cocktail-drinkers, and pre-dinner wine-lovers, as well as hosting
+							private events and mental health safe-space evenings
+						</p>
+						<p className="text-dark lg:text-xl text-lg mt-14 font-lato font-light tracking-wide lg:leading-relaxed leading-loose">
+							To see our current offerings{" "}
+							<Link href={"/#offers"} className="text-wonky">
+								click here.
+							</Link>
+						</p>
+					</div>
+				</section>
 
-        <section className="bg-darkbg">
-          <div className="container mx-auto flex flex-col lg:flex-row justify-between sm:mt-2 mt-10 px-4 md:py-24 py-14 gap-x-20">
-            <div className="lg:w-6/12 w-full h-full relative flex flex-col  my-auto ">
-              <p className="text-white lg:text-xl text-lg  font-lato font-light tracking-wide lg:leading-relaxed leading-loose">
-                The opportunity to take over The Wonky Labrador has also tapped
-                into Mac’s love of community, and you’ll find this a welcoming
-                place for all walks of life, including our four legged friends!
-                We’re accessible and LGBTQ+ friendly, breast-feeding supporters
-                - and we’ll always listen to your ideas for being a welcoming
-                space.
-              </p>
-              <p className="text-white lg:text-xl text-lg mt-14 font-lato font-light tracking-wide lg:leading-relaxed leading-loose">
-                Interested in being part of The Crafty One crew?
-              </p>
-              <p className="text-white lg:text-xl text-lg mt-14 font-lato font-light tracking-wide lg:leading-relaxed leading-loose">
-                Email:{" "}
-                <Link
-                  className="text-wonky"
-                  href="mailto:wonkylabrador@gmail.com"
-                >
-                  {" "}
-                  wonkylabrador@gmail.com
-                </Link>
-              </p>
-            </div>
-            <div className="lg:w-6/12 xl:w-4/12 w-full sm:h-[594px] h-[400px] relative mt-14 lg:mt-0">
-              <Image
-                src={Shop}
-                alt="Photo of shop front"
-                className="h-full w-full object-cover object-top rounded-md"
-                placeholder="blur"
-              />
-            </div>
-          </div>
-        </section>
-        <LogoList />
-        <Banner />
-      </main>
+				<section className="bg-wonky_green">
+					<div className="container mx-auto flex flex-col lg:flex-row justify-between sm:mt-2 mt-10 px-4 md:py-24 py-14 gap-x-20">
+						<div className="lg:w-6/12 w-full h-full relative flex flex-col  my-auto ">
+							<p className="text-white lg:text-xl text-lg  font-lato font-light tracking-wide lg:leading-relaxed leading-loose">
+								The opportunity to take over The Wonky Labrador has also tapped
+								into Mac’s love of community, and you’ll find this a welcoming
+								place for all walks of life, including our four legged friends!
+								We’re accessible and LGBTQ+ friendly, breast-feeding supporters
+								- and we’ll always listen to your ideas for being a welcoming
+								space.
+							</p>
+							<p className="text-white lg:text-xl text-lg mt-14 font-lato font-light tracking-wide lg:leading-relaxed leading-loose">
+								Interested in being part of The Crafty One crew?
+							</p>
+							<p className="text-white lg:text-xl text-lg mt-14 font-lato font-light tracking-wide lg:leading-relaxed leading-loose">
+								Email:{" "}
+								<Link
+									className="text-wonky"
+									href="mailto:wonkylabrador@gmail.com"
+								>
+									{" "}
+									wonkylabrador@gmail.com
+								</Link>
+							</p>
+						</div>
+						<div className="lg:w-6/12 xl:w-4/12 w-full sm:h-[594px] h-[400px] relative mt-14 lg:mt-0">
+							<Image
+								src={Shop}
+								alt="Photo of shop front"
+								className="h-full w-full object-cover object-top rounded-md"
+								placeholder="blur"
+							/>
+						</div>
+					</div>
+				</section>
+				<LogoList />
+				<Banner />
+			</main>
 
-      <Footer />
-    </div>
-  );
+			<Footer />
+		</div>
+	);
 };
 
 export default _about;
